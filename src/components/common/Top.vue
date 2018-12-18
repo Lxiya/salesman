3c2d4c13c2bf118c75e91da665d5621a3da4c379
Lxiya <1318961003@qq.com>
2018-12-18 10:12:54 +0800

    完善项目结构
    完善项目结构

---
+++ src/components/common/Top.vue
@@ -0,0 +1,14 @@
+<template>
+	
+</template>
+
+<script>
+export default {
+	name:'Top'
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
---
+++ src/components/index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+	<jdh-sale-top></jdh-sale-top>
+</template>
+
+<script>
+import Top from "components/commom/Top";
+
+export default {
+	name: "Index",
+	components: {
+		"jdh-sale-top": Top
+	}
+};
+</script>
+
+<style lang="stylus">
+</style>
